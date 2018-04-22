e766b20a363bc992f9fbe670e187721fc25446d4
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2018-04-22 11:26:05 +0300

    Removing additional scenes

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -4,10 +4,8 @@ import Categories from "./Categories";
 import CountryHome from "./CountryHome";
 import CategoryHome from "./CategoryHome";
 import Skeleton from "./Skeleton";
-import CountrySelectorScene from "./CountrySelectorScene";
-import LanguageSelectorScene from "./LanguageSelectorScene";
 import Services from "./Services";
 import Search from "./Search";
 import Selectors from "./Selectors";
 
-export { Home, Article, Categories, CountryHome, CategoryHome, Skeleton, CountrySelectorScene, LanguageSelectorScene, Search, Services, Selectors };
+export { Home, Article, Categories, CountryHome, CategoryHome, Skeleton,  Search, Services, Selectors };
