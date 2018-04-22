48ec4a9a79d1283ba22550f524de4b2ad593ad36
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2018-04-22 18:04:10 +0300

    Rey WIP

--- src/components/ArticlePage.js
+++ src/components/ArticlePage.js
@@ -1,7 +1,11 @@
-import React, { Component } from "react";
+import React, {
+    Component
+} from "react";
 import PropTypes from "prop-types";
 import "./ArticlePage.css";
-import { Helmet } from "react-helmet";
+import {
+    Helmet
+} from "react-helmet";
 import FacebookPlayer from "react-facebook-player";
 import YouTube from "react-youtube";
 import HeaderBar from "./HeaderBar";
@@ -13,119 +17,135 @@ import JsxParser from 'react-jsx-parser'
 const Remarkable = require("remarkable");
 
 const md = new Remarkable("full", {
-	html: true,
-	linkify: true,
-	breaks: true,
+    html: true,
+    linkify: true,
+    breaks: true,
 });
-
 
 /**
  *
  */
 export default class ArticlePage extends Component {
-	static propTypes = {
-		article: PropTypes.shape({
-			title: PropTypes.string,
-			hero: PropTypes.string,
-			body: PropTypes.string,
-		}),
-		category: PropTypes.shape({
-			name: PropTypes.string,
-			slug: PropTypes.string,
-			translations: PropTypes.array,
-		}),
-		onNavigate: PropTypes.func,
-	};
+    static propTypes = {
+        article: PropTypes.shape({
+            title: PropTypes.string,
+            hero: PropTypes.string,
+            body: PropTypes.string,
+        }),
+        category: PropTypes.shape({
+            name: PropTypes.string,
+            slug: PropTypes.string,
+            translations: PropTypes.array,
+        }),
+        onNavigate: PropTypes.func,
+    };
 
-	static contextTypes = {
-		config: PropTypes.object,
-	};
+    static contextTypes = {
+        config: PropTypes.object,
+    };
 
-	replaceLinks() {
-		const { onNavigate } = this.props;
+    replaceLinks() {
+        const {
+            onNavigate
+        } = this.props;
 
-		let hostname = "www.refugee.info";
-		if (global.location) {
-			hostname = global.location.hostname;
-		}
+        let hostname = "www.refugee.info";
+        if (global.location) {
+            hostname = global.location.hostname;
+        }
 
-		let anchors = Array.from(this._ref.querySelectorAll("a"));
-		anchors = anchors.filter(a => a.href.indexOf("http") || a.hostname === hostname || a.hostname === "www.refugee.info");
-		// eslint-disable-next-line
-		let isPhoneOrAlreadyProcessed = h => h.indexOf("#") === -1 && h.indexOf("tel:") === -1 && h.indexOf("mailto:") === -1;
+        let anchors = Array.from(this._ref.querySelectorAll("a"));
+        anchors = anchors.filter(a => a.href.indexOf("http") || a.hostname === hostname || a.hostname === "www.refugee.info");
+        // eslint-disable-next-line
+        let isPhoneOrAlreadyProcessed = h => h.indexOf("#") === -1 && h.indexOf("tel:") === -1 && h.indexOf("mailto:") === -1;
 
-		for (let anchor of anchors.filter(a => isPhoneOrAlreadyProcessed(a.href))) {
-			let href = anchor.href + "";
-			if (href.indexOf("http") >= 0) {
-				href =
-					"/" +
-					href
-						.split("/")
-						.slice(3)
-						.join("/");
-			}
-			// eslint-disable-next-line
-			anchor.href = "#";
-			anchor.onclick = () => {
-				onNavigate(href);
-				return false;
-			};
-		}
-	}
-	componentDidUpdate() {
-		this.replaceLinks();
-	}
+        for (let anchor of anchors.filter(a => isPhoneOrAlreadyProcessed(a.href))) {
+            let href = anchor.href + "";
+            if (href.indexOf("http") >= 0) {
+                href =
+                    "/" +
+                    href
+                    .split("/")
+                    .slice(3)
+                    .join("/");
+            }
+            // eslint-disable-next-line
+            anchor.href = "#";
+            anchor.onclick = () => {
+                onNavigate(href);
+                return false;
+            };
+        }
+    }
+    componentDidUpdate() {
+        this.replaceLinks();
+    }
 
-	componentDidMount() {
-		this.replaceLinks();
-	}
-	renderVideo() {
-		const { article } = this.props;
-		const { url } = article.fields;
-		const APP_ID = this.context.config.appId;
+    componentDidMount() {
+        this.replaceLinks();
+    }
+    renderVideo() {
+        const {
+            article
+        } = this.props;
+        const {
+            url
+        } = article.fields;
+        const APP_ID = this.context.config.appId;
 
-		if (/facebook.com/.test(url)) {
-			let videoId = url.replace(/.*facebook.com\/.*\/videos\/(.*)\/.*/, "$1");
+        if (/facebook.com/.test(url)) {
+            let videoId = url.replace(/.*facebook.com\/.*\/videos\/(.*)\/.*/, "$1");
 
-			return <FacebookPlayer className={"Facebook"} videoId={videoId} appId={APP_ID} />;
-		} else if (/youtube.com/) {
-			let videoId = url.replace(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/, "$7");
-			return <YouTube videoId={videoId} className={"YouTube"} />;
-		}
-		return null;
-	}
-	componentDidMount() {
-		/* */
-		const APP_ID = this.context.config.appId;
-		
-		Array.from(document.querySelectorAll('.yt') || []).forEach(e=> {
-			var videoId = e.innerHTML.replace(/<YouTube.*videoId=["']{(.*)}["'].*><\/YouTube>/gmi, '$1');
-			e.removeChild(e.firstChild);
-			ReactDOM.render(<YouTube videoId={videoId} className={"YouTube"} />, e);
-		});		
-		Array.from(document.querySelectorAll('.fb') || []).forEach(e=> {
-			var videoId = e.innerHTML.replace(/<FacebookPlayer.*videoId=["']{(.*)}["'].*><\/FacebookPlayer>/gmi, '$1');
-			e.removeChild(e.firstChild);
-			ReactDOM.render( <FacebookPlayer className={"Facebook"} videoId={videoId} appId={APP_ID} />, e);
-		});
-	}
+            return <FacebookPlayer className={"Facebook"} videoId={videoId} appId={APP_ID} />;
+        } else if (/youtube.com/) {
+            let videoId = url.replace(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/, "$7");
+            return <YouTube videoId={videoId} className={"YouTube"} />;
+        }
+        return null;
+    }
+    componentDidMount() {
+        /* */
+        const APP_ID = this.context.config.appId;
 
-	render() {
-		const { article, category, loading } = this.props;
-		const { title, content, hero, lead } = article.fields;
-		const { contentType } = article.sys;
+        Array.from(document.querySelectorAll('.yt') || []).forEach(e => {
+            var videoId = e.innerHTML.replace(/<YouTube.*videoId=["']{(.*)}["'].*><\/YouTube>/gmi, '$1');
+            e.removeChild(e.firstChild);
+            ReactDOM.render(<YouTube videoId={videoId} className={"YouTube"} />, e);
+        });
+        Array.from(document.querySelectorAll('.fb') || []).forEach(e => {
+            var videoId = e.innerHTML.replace(/<FacebookPlayer.*videoId=["']{(.*)}["'].*><\/FacebookPlayer>/gmi, '$1');
+            e.removeChild(e.firstChild);
+            ReactDOM.render(<FacebookPlayer className={"Facebook"} videoId={videoId} appId={APP_ID} />, e);
+        });
+    }
 
-		let html = md.render(content || lead);
-		html = html.replace(/(\+[0-9]{9,14}|00[0-9]{9,15})/g, `<a class="tel" href="tel:$1">$1</a>`);
-		html = html.replace(/(<YouTube.*\/>)/gmi, (a)=> {
-			return `<div class="yt">${a}</div>`;
-		});
-		html = html.replace(/(<FacebookPlayer.*\/>)/gmi, (a)=> {
-			return `<div class="fb">${a}</div>`;
-		});
+    render() {
+        const {
+            article,
+            category,
+            loading
+        } = this.props;
+        const {
+            title,
+            content,
+            hero,
+            lead
+        } = article.fields;
+        const {
+            contentType
+        } = article.sys;
 
-		return (
-			<div ref={r => (this._ref = r)} className={["ArticlePage", loading ? "loading" : "loaded"].join(" ")}>
+        let html = md.render(content || lead);
+        html = html.replace(/(\+[0-9]{9,14}|00[0-9]{9,15})/g, `<a class="tel" href="tel:$1">$1</a>`);
+        html = html.replace(/(<YouTube.*\/>)/gmi, (a) => {
+            return `<div class="yt">${a}</div>`;
+        });
+        html = html.replace(/(<FacebookPlayer.*\/>)/gmi, (a) => {
+            return `<div class="fb">${a}</div>`;
+        });
+
+        return (
+            <div ref={r => (this._ref = r)} className={["ArticlePage", loading ? "loading" : "loaded"].join(" ")}>
 				<Helmet>
 					<title>{title}</title>
 				</Helmet>
@@ -142,10 +162,10 @@ export default class ArticlePage extends Component {
 					)}
 				{contentType.sys.id === "video" && this.renderVideo()}
 				<article>
-					<AudioPlayer />
+					<AudioPlayer src={"/audio/background.ogg"}/>
 					<div dangerouslySetInnerHTML={{ __html: html }} />
 				</article>
 			</div>
-		);
-	}
+        );
+    }
 }
--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -6,15 +6,98 @@ import "./AudioPlayer.css";
 
 import ReactAudioPlayer from 'react-audio-player';
 
+const MIN_CYCLES = 2;
+
 class AudioPlayer extends React.Component {
+    state = {
+        run: 0,
+        cycle: 0,
+        playing: false,
+        maxCycles: MIN_CYCLES,
+
+    }
+    _refs = {};
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.animateVolume();
+        }, 500)
+    }
+
+    animateVolume() {
+        const {
+            run,
+            cycle,
+            maxCycles
+        } = this.state;
+        if (cycle < maxCycles) {
+            this.setState({
+                cycle: run == 2 ? cycle + 1 : cycle,
+                run: run == 2 ? 0 : run + 1
+            });
+
+            setTimeout(() => {
+                this.animateVolume();
+            }, 500)
+        }
+    }
+
+    playMe() {
+        const {
+            playing,
+        } = this.state;
+        if (!playing) {
+            this._refs.audioPlayer.audioEl.currentTime = 0;
+            this._refs.audioPlayer.audioEl.play();
+
+            this.setState({
+                playing: true,
+                cycle: 0,
+                run: 0,
+                maxCycles: 1e10
+            });
+
+            setTimeout(() => {
+                this.animateVolume();
+            }, 500)
+        } else {
+            this._refs.audioPlayer.audioEl.pause();
+        }
+    }
+
+    endPlay() {
+        this.setState({
+            playing: false,
+            cycle: 6,
+            run: 0,
+            maxCycles: MIN_CYCLES
+        });
+    }
+
     render() {
         const {
-            url
+            src
         } = this.props;
+
+        const {
+            run,
+        } = this.state;
+        const volumes = [
+            "fa-volume-up",
+            "fa-volume-down",
+            "fa-volume-off",
+        ]
+
         return (
-            <div><i className="material-icons">volume_up</i>
+            <div className="AudioPlayer"><i className={`fa ${volumes[run]}`} style={{ fontSize: 24, cursor: 'pointer'}} onClick={() => this.playMe() }></i>
             <ReactAudioPlayer
-            src="/audio/background.ogg"
+            ref={(r) => { this._refs.audioPlayer = r; }}
+            src={src}
+            style={{display: 'none'}}
+            onPlay={()=> { }}
+            onEnded={()=>{this.endPlay()}}
+            onAborted={()=>{this.endPlay()}}
+            onPause={()=>{this.endPlay()}}
             />
             </div>
         )
