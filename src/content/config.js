f91c1172dfbea5eca1a812aab6855a024a952883
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2018-04-22 20:49:52 +0300

    Beta and others

--- src/content/config.js
+++ src/content/config.js
@@ -33,5 +33,20 @@ module.exports = {
 		gaTracker: "UA-68574649-7",
 		appId: "154471931830995",
 		thumbnail: "/images/logos/ki-thumbnail.png",
+	},
+	"demo.khabrona.info": {
+		theme: "ki",
+		accessToken: "55bea7f7ed5d0e812aa1d48e378dd9a2edb8f9f78c17bd9d2ffa6a014ea3ee2b",
+		space: "l4oa58axtceh",
+		languages: [["en", "English"], ["ar", "العربيـة"]],
+		logo: "/ki-new-logo.png",
+		title: "Khabrona.Info",
+		disableCountrySelector: true,
+		questionLink: "mailto:jo-khabrona-info@mercycorps.org",
+		hideServiceMap: true,
+		favicon: "/ki-favicon.png",
+		gaTracker: "UA-68574649-7",
+		appId: "154471931830995",
+		thumbnail: "/images/logos/ki-thumbnail.png",
 	}
 };
